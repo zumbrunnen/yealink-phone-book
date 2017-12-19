619d10dd54eb04f65bdfe6846f4fdeec125475b2
David Zumbrunnen <zumbrunnen@gmail.com>
2017-12-19 15:50:57 +0100

    I18n (messages, alerts) and phone book name validation

--- app/controllers/phone_book_entries_controller.rb
+++ app/controllers/phone_book_entries_controller.rb
@@ -32,7 +32,8 @@ class PhoneBookEntriesController < ApplicationController
 
     respond_to do |format|
       if @phone_book_entry.save
-        format.html { redirect_to @phone_book_entry, notice: 'Phone book entry was successfully created.' }
+        notice = t('successful.messages.created', model: PhoneBookEntry.model_name.human)
+        format.html { redirect_to @phone_book_entry, notice: notice }
         format.json { render :show, status: :created, location: @phone_book_entry }
       else
         format.html { render :new }
@@ -46,7 +47,8 @@ class PhoneBookEntriesController < ApplicationController
   def update
     respond_to do |format|
       if @phone_book_entry.update(phone_book_entry_params)
-        format.html { redirect_to @phone_book_entry, notice: 'Phone book entry was successfully updated.' }
+        notice = t('successful.messages.updated', model: PhoneBookEntry.model_name.human)
+        format.html { redirect_to @phone_book_entry, notice: notice }
         format.json { render :show, status: :ok, location: @phone_book_entry }
       else
         format.html { render :edit }
@@ -60,7 +62,8 @@ class PhoneBookEntriesController < ApplicationController
   def destroy
     @phone_book_entry.destroy
     respond_to do |format|
-      format.html { redirect_to phone_book_entries_url, notice: 'Phone book entry was successfully destroyed.' }
+      notice = t('successful.messages.deleted', model: PhoneBookEntry.model_name.human)
+      format.html { redirect_to phone_book_entries_url, notice: notice }
       format.json { head :no_content }
     end
   end
--- app/models/phone_book.rb
+++ app/models/phone_book.rb
@@ -1,5 +1,5 @@
 class PhoneBook < ApplicationRecord
   has_many :phone_book_entries
 
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true, format: { with: /\A[a-z\s]+\z/i }
 end
